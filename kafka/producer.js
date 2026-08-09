2e85b5157f917adf309498bc7007058a4869d8f3
Richa Meshram <richameshram1988@gmail.com>
2026-08-09 06:39:00 +0530

    Connect Aiven Kafka

--- kafka/producer.js
+++ kafka/producer.js
@@ -1,31 +1,40 @@
 const { Kafka } = require("kafkajs");
 
 const kafka = new Kafka({
-    clientId: "inventory-app",
-    brokers: ["127.0.0.1:9092"],
+  clientId: "inventory-app",
+
+  brokers: [process.env.KAFKA_BROKER],
+
+  ssl: true,
+
+  sasl: {
+    mechanism: "plain",
+    username: process.env.KAFKA_USERNAME,
+    password: process.env.KAFKA_PASSWORD,
+  },
 });
 
 const producer = kafka.producer();
 
 const connectProducer = async () => {
-    await producer.connect();
-    console.log("Kafka Producer Connected");
+  await producer.connect();
+  console.log("Kafka Producer Connected");
 };
 
 const sendInventoryEvent = async (event) => {
-    await producer.send({
-        topic: "inventory-events",
-        messages: [
-            {
-                value: JSON.stringify(event),
-            },
-        ],
-    });
+  await producer.send({
+    topic: process.env.KAFKA_TOPIC || "inventory-events",
+    messages: [
+      {
+        value: JSON.stringify(event),
+      },
+    ],
+  });
 
-    console.log("Event sent:", event);
+  console.log("Event sent:", event);
 };
 
 module.exports = {
-    connectProducer,
-    sendInventoryEvent,
+  connectProducer,
+  sendInventoryEvent,
 };
